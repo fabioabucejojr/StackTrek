b15b31b9e0ef98ede217a8c73f60168218430103
fabioabucejojr <24905746+fabioabucejojr@users.noreply.github.com>
2022-08-05 13:25:58 +0800

    Create 1_pa_Mine.py

---
+++ 1 Python/Chapter 3/1_pa_Mine.py
@@ -0,0 +1,10 @@
+weightof_dresses = 50
+weightof_organizers = 125
+dresses = input("How many dresses you want to buy?: ")
+
+organizers = input("How many organizers you want to buy?: ")
+
+sum1 = int(dresses) * int(weightof_dresses)
+sum2 = int(organizers) * int(weightof_organizers)
+      
+print(sum1 + sum2)
